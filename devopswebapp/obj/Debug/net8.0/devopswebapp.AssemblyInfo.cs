86e8d03216878e305bda5310dd722292c0a595f3
faizul.khan <faizul.khan@dngroup.com>
2025-04-16 10:47:54 +0600

    first commit2

--- devopswebapp/obj/Debug/net8.0/devopswebapp.AssemblyInfo.cs
+++ devopswebapp/obj/Debug/net8.0/devopswebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("devopswebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5e937f6902f5bcbd4ba577ac4ae6376842adbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("devopswebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("devopswebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
